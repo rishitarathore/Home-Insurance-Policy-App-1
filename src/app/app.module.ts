1338defb70212706285091f34fcfcf5ac036715c
rishitarathore <rishitarathore7@gmail.com>
2021-10-06 15:20:09 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { HeadingComponent } from './heading/heading.component';
+import { ContentComponent } from './content/content.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressBarComponent,
+    HeadingComponent,
+    ContentComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
